f7c02f18b2f5b4f6db4403eb79d11bce960d8cc9
Turker CALISKAN <turker.caliskan90@gmail.com>
2024-03-21 15:05:38 +0100

    oui

--- src/Pion.java
+++ src/Pion.java
@@ -17,17 +17,19 @@ public class Pion {
         return this.couleur;
     }
 
+    public Pion(){}
+
     public Pion(char coul,int X, int Y){
         this.couleur = coul;
         this.posX = X;
         this.posY = Y;
     }
 
-    public void setPos(int x, int y){
+    public void setPos(int X, int Y){
         this.posX = X;
         this.posY = Y;
     }
-    public void setPosX(int x){
+    public void setPosX(int X){
         this.posX = X;
     }
 
