725b235dbc83512a098a22c41f1fc352a63fc18e
rcaruhel <remyca.caruhel@gmail.com>
2024-03-21 14:38:41 +0100

    Ajout de la classe Pion

--- src/Pion.java
+++ src/Pion.java
@@ -1,4 +1,38 @@
 package src;
 
 public class Pion {
+    private char couleur;
+    private int posX;
+    private int posX;
+
+    public int getPosX(){
+        return this.posX;
+    }
+
+    public int getPosY(){
+        return this.posY;
+    }
+
+    public char getCouleur(){
+        return this.couleur;
+    }
+
+    public Pion(char coul,int X, int Y){
+        this.couleur = coul;
+        this.posX = X;
+        this.posY = Y;
+    }
+
+    public void setPos(int x, int y){
+        this.posX = X;
+        this.posY = Y;
+    }
+    public void setPosX(int x){
+        this.posX = X;
+    }
+
+    public void setPosY(int y){
+        this.posY = y;
+    }
+
 }
