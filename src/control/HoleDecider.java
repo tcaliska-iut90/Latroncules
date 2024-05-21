a84f3ea6c68154c935cfbc2f78341e3bb84305c6
Enzo Vidy <enzo.vidy@edu.univ-fcomte.fr>
2024-05-21 10:56:24 +0200

    Ajout classe PointPosition + ajout cases stratégiques 1ere IA

--- src/control/HoleDecider.java
+++ src/control/HoleDecider.java
@@ -5,6 +5,7 @@ import boardifier.control.Controller;
 import boardifier.control.Decider;
 import boardifier.model.GameElement;
 import boardifier.model.Model;
+import boardifier.model.PointPosition;
 import boardifier.model.action.ActionList;
 import model.HoleBoard;
 import model.HoleStageModel;
@@ -32,38 +33,35 @@ public class HoleDecider extends Decider {
         int rowDest; // the dest. row in board
         int colDest; // the dest. col in board
 
-        /*
-        1ère IA (position stratégique et minimisation des risques) :
 
-        Initialisation :
-        ◦ Créer une liste de tous les mouvements valides pour le joueur actuel.
-        ◦ Initialiser une liste de positions stratégiques sur le plateau (par exemple, les cases centrales).
+        //1ère IA (position stratégique et minimisation des risques) :
+        //Initialisation :
+        //Créer une liste de tous les mouvements valides pour le joueur actuel.
+        //List<GameElement> pawns = board.getPawns(model.getCurrentPlayer());
+        ActionList actions = new ActionList();
 
-                Évaluation des mouvements :
-        ◦ Pour chaque mouvement valide, évaluer la position finale du pion.
-        ◦ Attribuer un score à chaque mouvement basé sur plusieurs critères :
-            ▪ Contrôle des cases centrales : Les mouvements qui placent un pion sur une case centrale obtiennent un score plus élevé.
-            ▪ Proximité des pions adverses : Les mouvements qui rapprochent un pion d'un pion adverse obtiennent un score plus élevé, favorisant les futures opportunités de capture.
-            ▪ Éloignement des bords : Les mouvements qui placent un pion loin des bords du plateau peuvent être favorisés pour éviter les pièges.
-            ▪ Sécurité : Les mouvements qui placent un pion dans une position moins susceptible d'être capturé au tour suivant obtiennent un score plus élevé.
+        //Initialiser une liste de positions stratégiques sur le plateau (par exemple, les cases centrales).
+        PointPosition[] strategicPositions = new PointPosition[] {
+                new PointPosition(2,2), new PointPosition(3,2), new PointPosition(4,2), new PointPosition(5,2),
+                new PointPosition(2,3), new PointPosition(3,3), new PointPosition(4,3), new PointPosition(5,3),
+                new PointPosition(2,4), new PointPosition(3,4), new PointPosition(4,4), new PointPosition(5,4),
+                new PointPosition(2,5), new PointPosition(3,5), new PointPosition(4,5), new PointPosition(5,5)
+        };
 
-        Sélection du mouvement :
-        ◦ Choisir le mouvement avec le score le plus élevé parmi les mouvements évalués.
-        ◦ Exécuter le mouvement sélectionné.
-        */
+        // Évaluation des mouvements :
+        //  ◦ Pour chaque mouvement valide, évaluer la position finale du pion.
+        //  ◦ Attribuer un score à chaque mouvement basé sur plusieurs critères :
+        //    ▪ Contrôle des cases centrales : Les mouvements qui placent un pion sur une case centrale obtiennent un score plus élevé.
+        //    ▪ Proximité des pions adverses : Les mouvements qui rapprochent un pion d'un pion adverse obtiennent un score plus élevé, favorisant les futures opportunités de capture.
+        //    ▪ Éloignement des bords : Les mouvements qui placent un pion loin des bords du plateau peuvent être favorisés pour éviter les pièges.
+        //    ▪ Sécurité : Les mouvements qui placent un pion dans une position moins susceptible d'être capturé au tour suivant obtiennent un score plus élevé.
 
-        // get the list of all pawns that can be moved
-        List<GameElement> pawns = board.getElements(model.getIdPlayer());
-        // get the list of all possible moves for each pawn
-        ActionList actions = ActionFactory.generatePutInContainer(model, pawns.get(0), "holeboard", 0, 0);
-        for (GameElement p : pawns) {
-            List<Point> moves = board.getValidMoves(p);
-            for (Point move : moves) {
-                actions.add(ActionFactory.generatePutInContainer(model, p, "holeboard", move.y, move.x));
-            }
-        }
+        //Sélection du mouvement :
+        //◦ Choisir le mouvement avec le score le plus élevé parmi les mouvements évalués.
+        //◦ Exécuter le mouvement sélectionné.
 
-        ActionList actions = ActionFactory.generatePutInContainer( model, pawn, "holeboard", rowDest, colDest);
+
+        //ActionList actions = ActionFactory.generatePutInContainer( model, pawn, "holeboard", rowDest, colDest);
         //actions.setDoEndOfTurn(true); // after playing this action list, it will be the end of turn for current player.
 
         return actions;
